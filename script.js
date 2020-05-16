d450c655af26c8e8db447cd1b26b0def8f08c640
yusufabukar <65050809+yusufabukar@users.noreply.github.com>
2020-05-16 19:40:34 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,69 @@
+var css = document.querySelector('h3');
+var colour1 = document.querySelector('.colour1');
+var colour2 = document.querySelector('.colour2');
+var body = document.getElementById('gradient');
+
+function displayCSSValue() {
+	css.textContent = body.style.background + ';';
+}
+
+function setGradient() {
+	body.style.background = 'linear-gradient(to right, ' + colour1.value + ', ' + colour2.value + ')';
+
+	displayCSSValue();
+}
+
+colour1.addEventListener('input', setGradient);
+colour2.addEventListener('input', setGradient);
+
+// Random button colour-picker
+
+var button = document.querySelector('button');
+
+function getRandomInteger(min, max) {
+	return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function getRandomColour() {
+	// var r = Math.floor(Math.random() * 255);
+	// var g = Math.floor(Math.random() * 255);
+	// var b = Math.floor(Math.random() * 255);
+	// return 'rgb(' + r + ', ' + g + ', ' + b + ')';
+
+	var hexCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+	var r1 = hexCharacters[getRandomInteger(0, 15)];
+	var r2 = hexCharacters[getRandomInteger(0, 15)];
+	var g1 = hexCharacters[getRandomInteger(0, 15)];
+	var g2 = hexCharacters[getRandomInteger(0, 15)];
+	var b1 = hexCharacters[getRandomInteger(0, 15)];
+	var b2 = hexCharacters[getRandomInteger(0, 15)];
+
+	return '#' + r1 + r2 + g1 + g2 + b1 + b2;
+}
+
+function setRandomColour() {
+	var tempColour1 = getRandomColour();
+	var tempColour2 = getRandomColour();
+
+	body.style.background = 'linear-gradient(to right, ' + tempColour1 + ', ' + tempColour2 + ')';
+	displayCSSValue();
+
+	return [tempColour1, tempColour2];
+}
+
+button.addEventListener('click', setRandomColour);
+
+// Set random background on page load
+
+var tempColours = setRandomColour();
+
+// Set input colour to match background on page load
+
+var inputValues = document.querySelectorAll('input');
+inputValues[0] = inputValues[0].setAttribute('value', tempColours[0]);
+inputValues[1] = inputValues[1].setAttribute('value', tempColours[1]);
+
+
+// Display inital CSS linear-gradient property on page load
+
+displayCSSValue();
