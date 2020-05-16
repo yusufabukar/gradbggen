5f430b2f6e0341dc07162fdd28cbb1d6cc801d21
yusufabukar <65050809+yusufabukar@users.noreply.github.com>
2020-05-16 20:11:01 +0100

    Stylistic changes
    Random button has a reverse effect on hover
    Input selector colours update when random button pressed

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Gradient picker
+
 var css = document.querySelector('h3');
 var colour1 = document.querySelector('.colour1');
 var colour2 = document.querySelector('.colour2');
@@ -9,7 +11,6 @@ function displayCSSValue() {
 
 function setGradient() {
 	body.style.background = 'linear-gradient(to right, ' + colour1.value + ', ' + colour2.value + ')';
-
 	displayCSSValue();
 }
 
@@ -30,6 +31,8 @@ function getRandomColour() {
 	// var b = Math.floor(Math.random() * 255);
 	// return 'rgb(' + r + ', ' + g + ', ' + b + ')';
 
+
+	// Could possibly be placed in a loop
 	var hexCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
 	var r1 = hexCharacters[getRandomInteger(0, 15)];
 	var r2 = hexCharacters[getRandomInteger(0, 15)];
@@ -47,6 +50,8 @@ function setRandomColour() {
 
 	body.style.background = 'linear-gradient(to right, ' + tempColour1 + ', ' + tempColour2 + ')';
 	displayCSSValue();
+	// Update input colours when button is pressed
+	updateInputColours(tempColour1, tempColour2);
 
 	return [tempColour1, tempColour2];
 }
@@ -59,10 +64,13 @@ var tempColours = setRandomColour();
 
 // Set input colour to match background on page load
 
-var inputValues = document.querySelectorAll('input');
-inputValues[0] = inputValues[0].setAttribute('value', tempColours[0]);
-inputValues[1] = inputValues[1].setAttribute('value', tempColours[1]);
+function updateInputColours(input1, input2) {
+	var inputValues = document.querySelectorAll('input');
+	inputValues[0] = inputValues[0].setAttribute('value', input1);
+	inputValues[1] = inputValues[1].setAttribute('value', input2);
+}
 
+updateInputColours(tempColours[0], tempColours[1]);
 
 // Display inital CSS linear-gradient property on page load
 
